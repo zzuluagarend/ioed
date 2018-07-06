6797af8ba463aa3f45de16fdb728fcfc2dee4c99
[zzuluagarend] <zzuluagar@gmail.com>
2018-07-06 12:34:15 +0200

    updates

--- config/config_deploy.js
+++ config/config_deploy.js
@@ -1,42 +1,19 @@
 var config_deploy = {
-    
+
     // obligatory fields
-	
-    // needed to recover data from server app
-    "author": "RandomJane",
-    // needed to recover data from server app
-    "experiment_id": "MinimalTemplateDEMO", 
-    "description": "A minimal template for a browser-based experiment",
+
+    // the experimentID is needed to recover data from the _babe server app
+	// you receive the experimentID when you create the experiment using the _babe server app
+
+	 "experimentID": "7",
 
     // set deployment method; use one of:
-    //'debug', 'localServer', 'MTurk', 
+    //'debug', 'localServer', 'MTurk',
     // 'MTurkSandbox', 'Prolific', 'directLink'
-    "deployMethod" : "debug", 
-    
+    "deployMethod" : "directLink",
+
     // optional fields
-    
+
     // who to contact in case of trouble
-    "contact_email": "YOUREMAIL@wherelifeisgreat.you", 
+    "contact_email": "YOUREMAIL@wherelifeisgreat.you",
 };
-
-// user does not (should not) change the following information
-
-// checks the config _deploy.deployMethod is MTurk or MTurkSandbox,
-// sets the submission url to MTukr's servers
-config_deploy.MTurk_server = config_deploy.deployMethod == "MTurkSandbox" ?
-    "https://workersandbox.mturk.com/mturk/externalSubmit" : // URL for MTurk sandbox
-    config_deploy.deployMethod == 'MTurk' ?
-    "https://www.mturk.com/mturk/externalSubmit" : // URL for live HITs on MTurk
-    ""; // blank if deployment is not via MTurk
-// if the config_deploy.deployMethod is not debug, then liveExperiment is true
-config_deploy.liveExperiment = config_deploy.deployMethod !== "debug";
-config_deploy.prolificCode = '9BTAOPQD';
-config_deploy.is_MTurk = config_deploy.MTurk_server !== "";
-console.log("deployMethod: " + config_deploy.deployMethod);
-console.log("live experiment: " + config_deploy.liveExperiment);
-console.log("runs on MTurk: " + config_deploy.is_MTurk);
-console.log("MTurk server: " + config_deploy.MTurk_server);
-
-
-
-   
--- scripts/submit_to_server.js
+++ scripts/submit_to_server.js
@@ -3,7 +3,7 @@
 // 1) isMTurk - boolean; true if the experiment runs on MTurk
 // 2) contactEmail - string
 // 3) trials
-var submitResults = function(contactEmail, data) {
+var submitResults = function(contactEmail, submissionURL, data) {
     // if isMTurk is not given, sets it to false
     isMTurk = typeof isMTurk !== 'undefined' ? isMTurk : false;
     // set a default contact email
@@ -11,7 +11,7 @@ var submitResults = function(contactEmail, data) {
 
     $.ajax({
         type: 'POST',
-        url: 'https://procomprag.herokuapp.com/api/submit_experiment',
+        url: submissionURL,
         crossDomain: true,
         contentType: 'application/json',
         data: JSON.stringify(data),
@@ -45,37 +45,16 @@ var submitResults = function(contactEmail, data) {
                 if (textStatus == "timeout") {
                     alert("Oops, the submission timed out. Please try again. If the problem persists, please contact " + contactEmail + ", including your ID");
                 } else {
-                    alert("Oops, the submission failed. Please try again. If the problem persists, please contact " + contactEmail + ", including your ID");
+                    alert("Oops, the submission failed. The server says: " + responseData.responseText + "\nPlease try again. If the problem persists, please contact " + contactEmail + "with this error message, including your ID");
                 }
             }
         }
     });
 };
 
-// submits to MTurk's servers if config.is_MTurk is set to true
+// submits to MTurk's servers
 // and the correct url is given in config.MTurk_server
 var submitToMTurk = function() {
     var form = $('#mturk-submission-form');
-    console.log(form.attr('action'));
-
-    console.log('submits to mturk');
     form.submit();
-};
-
-
-// parses the url to get thr assignment_id and worker_id
-// if the exp is run on MTurk
-var getHITData = function() {
-    var url = window.location.href;
-    var qArray = url.split('?');
-    qArray = qArray[1].split('&');
-    var HITData = {};
-
-    for (var i=0; i<qArray.length; i++) {
-        HITData[qArray[i].split('=')[0]] = qArray[i].split('=')[1];
-    }
-
-    console.log(HITData);
-
-    return HITData;
 };
